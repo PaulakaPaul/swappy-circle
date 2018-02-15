0c59a8c41b350da95b56fc6ad68b52e3d91cb7d8
PaulAkaPaul <p.e.iusztin@gmail.com>
2018-02-15 13:28:03 +0200

    routine

--- CircleGameFB/Assets/Scripts/DataType/NotFoundException.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System;
-
-public class NotFoundException : Exception {
-	public NotFoundException(string message) : base (message) { } // : base (message) calls the super constructor
-}
